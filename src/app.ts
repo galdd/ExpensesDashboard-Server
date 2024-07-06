aea017cd4862aaf9505de4ccd08868a2b5c3f921
Gal Dagan <gald2345@gmail.com>
2024-07-06 18:21:59 +0300

    Add prod, dev and test mode

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import config from "../config/config";
 import { routes } from "./routes";
 
 dotenv.config();
@@ -9,6 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies using query-string library
+
+if (config.isProduction) {
+  app.use(helmet());
+  // app.use(morgan("combined"));
+} else {
+  app.use(morgan("dev"));
+}
+
 routes(app);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+import https from 'https';
 import http from 'http';
+import fs from 'fs';
 import app from "./app";
 import { connect } from "./db";
 import { initSocket } from "./services/socket";
+import config from "../config/config";
 
-const port = process.env.PORT || 1337;
-const server = http.createServer(app);
+const createServer = () => {
+  if (config.isProduction && config.ssl) {
+    const sslOptions = {
+      key: fs.readFileSync(config.ssl.key),
+      cert: fs.readFileSync(config.ssl.cert),
+    };
+    return https.createServer(sslOptions, app);
+  }
+  return http.createServer(app);
+};
 
-server.listen(port, async () => {
-  console.log("Server is running on port:", port);
-  await connect(process.env.DBUri as string);
+const server = createServer();
+
+server.listen(config.port, async () => {
+  console.log(`Server is running on port: ${config.port}`);
+  await connect(config.dbUri);
   await initSocket(server); // Initialize Socket.IO
 });
